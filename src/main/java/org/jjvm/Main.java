04de0f05b63ceadb72d1d522609e7fba19df8b06
Cijie Xia <cijie.xia@mail.utoronto.ca>
2022-05-22 15:40:43 +0800

    Init

---
+++ src/main/java/org/jjvm/Main.java
@@ -0,0 +1,26 @@
+package org.jjvm;
+
+import org.apache.commons.cli.*;
+import org.jjvm.cmd.CMD;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        CMD cmd = CMD.parseCommandLineArguments(args);
+        if (cmd.versionFlag) {
+            System.out.println("version 0.0.1");
+        }
+        else if (cmd.helpFlag || cmd.klass.equals("")) {
+            CMD.printCommandLineUsage();
+        }
+        else {
+            startJVM(cmd);
+        }
+    }
+
+    public static void startJVM(CMD cmd) {
+        System.out.println("Start JVM ...");
+        for (String arg : cmd.args) {
+            System.out.println(arg);
+        }
+    }
+}
---
+++ src/main/java/org/jjvm/cmd/CMD.java
@@ -0,0 +1,63 @@
+package org.jjvm.cmd;
+
+import org.apache.commons.cli.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * The class for command line options
+ */
+public class CMD {
+    static class JavaCMDOption {
+        public static final String HELP_FULL = "help";
+        public static final String HELP_SHORT = "?";
+        public static final String VERSION = "version";
+        public static final String CLASSPATH_FULL = "classpath";
+        public static final String CLASSPATH_SHORT = "cp";
+    }
+
+    public boolean helpFlag = false; // print help message
+    public boolean versionFlag = false; // print version info
+    public String cpOption = "";
+    public String klass = "";
+    public ArrayList<String> args = new ArrayList<>();
+
+    public static Options getCommandLineOptions() {
+        Options options = new Options();
+        options.addOption(JavaCMDOption.HELP_FULL,"print help message");
+        options.addOption(JavaCMDOption.HELP_SHORT, "print help message");
+        options.addOption(JavaCMDOption.VERSION, "print version and exit");
+        options.addOption(JavaCMDOption.CLASSPATH_FULL, "classpath");
+        options.addOption(JavaCMDOption.CLASSPATH_SHORT, "classpath");
+        return options;
+    }
+
+    public static void printCommandLineUsage() {
+        HelpFormatter formatter = new HelpFormatter();
+        String header = "JJVM \n\n";
+        String footer = "\nAuthor: Cijie Xia";
+        formatter.printHelp("jjava", header, getCommandLineOptions(), footer, true);
+    }
+
+    public static CMD parseCommandLineArguments(String[] args) throws ParseException {
+        Options options = getCommandLineOptions();
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine c = parser.parse(options, args);
+
+        CMD cmd = new CMD();
+        cmd.helpFlag = c.hasOption(JavaCMDOption.HELP_FULL) || c.hasOption(JavaCMDOption.HELP_SHORT);
+        cmd.versionFlag = c.hasOption(JavaCMDOption.VERSION);
+
+        if (c.hasOption(JavaCMDOption.CLASSPATH_FULL)) {
+            cmd.klass = c.getOptionValue(JavaCMDOption.CLASSPATH_FULL);
+        }
+        else if (c.hasOption(JavaCMDOption.CLASSPATH_SHORT)){
+            cmd.klass = c.getOptionValue(JavaCMDOption.CLASSPATH_SHORT);
+        }
+
+        cmd.args.addAll(Arrays.asList(c.getArgs()));
+        return cmd;
+    }
+}
